40a9f72949d46cfb6ebeaf53da7cdb046ad387a6
Chris Berthold <cberthold@technodotnet.com>
2015-05-05 21:18:24 -0400

    add solution files from demo

---
+++ 2015-04-30-IoT-Using-Raspberry-PI-2-CSharp/HelloWorld/BlinkyLight/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Raspberry.IO.GeneralPurpose;
+using Raspberry.IO.GeneralPurpose.Behaviors;
+using System.Threading;
+
+namespace BlinkyLight
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var led = ConnectorPin.P1Pin7.Output();
+
+			var connection = new GpioConnection (led);
+
+			for (var i = 0; i < 100; i++) {
+				connection.Toggle (led);
+				Thread.Sleep (250);
+			}
+		}
+	}
+}
