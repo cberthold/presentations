50ba26d9f7f216c5a52b47a249cfb08a957349b5
Chris Berthold <cberthold@technodotnet.com>
2019-02-10 13:33:23 -0500

    create commands to be passed in mediatr

---
+++ 2019-03-02_Introduction_to_Event_Sourcing_and_CQRS/frontend/Logic/Commands/WithdrawalCommand.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using frontend.Models;
+
+namespace frontend.Logic.Commands
+{
+    public class WithdrawalCommand
+    {
+
+    }
+}
