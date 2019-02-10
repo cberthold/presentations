575bfd733ce2312afe3b846ecb0ed438e76e3b56
Chris Berthold <cberthold@technodotnet.com>
2019-02-10 18:56:46 -0500

    have withdrawals create in database

--- 2019-03-02_Introduction_to_Event_Sourcing_and_CQRS/frontend/Logic/CommandHandlers/WithdrawalCommandHandlers.cs
+++ 2019-03-02_Introduction_to_Event_Sourcing_and_CQRS/frontend/Logic/CommandHandlers/WithdrawalCommandHandlers.cs
@@ -6,19 +6,37 @@ using System.Threading.Tasks;
 using frontend.Models;
 using frontend.Logic.Commands;
 using MediatR;
+using frontend.Data;
 
 namespace frontend.Logic.CommandHandlers
 {
     public class WithdrawalCommandHandler : IRequestHandler<WithdrawalCommand, TransactionResponse>
     {
-        public Task<TransactionResponse> Handle(WithdrawalCommand request, CancellationToken cancellationToken)
+        private readonly IBankAccountsContext context;
+
+        public WithdrawalCommandHandler(IBankAccountsContext context)
         {
+            this.context = context;
+        }
+
+        public async Task<TransactionResponse> Handle(WithdrawalCommand request, CancellationToken cancellationToken)
+        {
+            var withdrawal = new Withdrawal
+            {
+                Date = DateTime.Today,
+                Amount = request.Amount,
+                AccountId = request.AccountId,
+            };
+
+            context.Withdrawals.Add(withdrawal);
+            await context.SaveChangesAsync(cancellationToken);
+
             var response = new TransactionResponse {
                 Type = "Withdrawal",
                 Amount = request.Amount,
             };
 
-            return Task.FromResult(response);
+            return response;
         }
 
     }
