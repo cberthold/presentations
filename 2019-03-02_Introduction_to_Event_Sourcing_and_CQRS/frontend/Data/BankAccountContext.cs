922f049926a7c14310309cffc03751724beaadac
Chris Berthold <cberthold@technodotnet.com>
2019-02-10 15:32:03 -0500

    add basic bank account db context

---
+++ 2019-03-02_Introduction_to_Event_Sourcing_and_CQRS/frontend/Data/BankAccountContext.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace frontend.Data
+{
+    public class BankAccountsContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BankAccountsContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Deposit> Deposits { get; set; }
+    }
+
+    public class Account
+    {
+        public Guid AccountId { get; set; }
+        public string AccountName { get; set; }
+        public decimal CurrentBalance { get; set; }
+
+        public List<Deposit> Deposits { get; set; }
+        public List<Withdrawal> Withdrawals { get; set; }
+    }
+
+    public class Deposit
+    {
+        public Guid DepositId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+    }
+
+    public class Withdrawal
+    {
+        public Guid WithdrawalId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+    }
+}
