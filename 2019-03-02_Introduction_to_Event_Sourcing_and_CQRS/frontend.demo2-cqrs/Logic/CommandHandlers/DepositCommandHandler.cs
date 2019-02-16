f1ff032ec8f2627fb28d6622ccfaa4676a3e8115
Chris Berthold <cberthold@technodotnet.com>
2019-02-16 13:32:22 -0500

    copy project and make cqrs branch

---
+++ 2019-03-02_Introduction_to_Event_Sourcing_and_CQRS/frontend.demo2-cqrs/Logic/CommandHandlers/DepositCommandHandler.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using frontend.Models;
+using frontend.Logic.Commands;
+using MediatR;
+using frontend.Data;
+
+namespace frontend.Logic.CommandHandlers
+{
+    public class DepositCommandHandler : IRequestHandler<DepositCommand, TransactionResponse>
+    {
+        private readonly IBankAccountsContext context;
+
+        public DepositCommandHandler(IBankAccountsContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<TransactionResponse> Handle(DepositCommand request, CancellationToken cancellationToken)
+        {
+            var deposit = new Deposit
+            {
+                Date = DateTime.Today,
+                Amount = request.Amount,
+                AccountId = request.AccountId,
+            };
+
+            using (var tx = await context.Database.BeginTransactionAsync(cancellationToken))
+            {
+
+                context.Deposits.Add(deposit);
+                await context.SaveChangesAsync(cancellationToken);
+
+                tx.Commit();
+
+                var response = new TransactionResponse
+                {
+                    Type = "Deposit",
+                    Amount = request.Amount,
+                };
+
+                return response;
+            }
+
+        }
+    }
+}
---
+++ 2019-03-02_Introduction_to_Event_Sourcing_and_CQRS/frontend.demo2-cqrs/Logic/CommandHandlers/WithdrawalCommandHandlers.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using frontend.Models;
+using frontend.Logic.Commands;
+using MediatR;
+using frontend.Data;
+
+namespace frontend.Logic.CommandHandlers
+{
+    public class WithdrawalCommandHandler : IRequestHandler<WithdrawalCommand, TransactionResponse>
+    {
+        private readonly IBankAccountsContext context;
+
+        public WithdrawalCommandHandler(IBankAccountsContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<TransactionResponse> Handle(WithdrawalCommand request, CancellationToken cancellationToken)
+        {
+            var withdrawal = new Withdrawal
+            {
+                Date = DateTime.Today,
+                Amount = request.Amount,
+                AccountId = request.AccountId,
+            };
+            using (var tx = await context.Database.BeginTransactionAsync(cancellationToken))
+            {
+                context.Withdrawals.Add(withdrawal);
+                await context.SaveChangesAsync(cancellationToken);
+
+                tx.Commit();
+
+                var response = new TransactionResponse
+                {
+                    Type = "Withdrawal",
+                    Amount = request.Amount,
+                };
+
+                return response;
+            }
+        }
+
+    }
+}
