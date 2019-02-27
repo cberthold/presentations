fe61cde833238021a813db2ad74fc510da29fd60
Chris Berthold <cberthold@technodotnet.com>
2019-02-27 03:23:36 -0500

    create basic account aggregate with a deposit method and ability to add a new one

---
+++ 2019-03-02_Introduction_to_Event_Sourcing_and_CQRS/frontend.demo3-es/Logic/DomainAggregates/AccountAggregate.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CQRSlite.Domain;
+using frontend.Data;
+
+namespace frontend.Logic.DomainEvents
+{
+    public class AccountAggregate : AggregateRoot
+    {
+        public Guid AccountId { get; private set; }
+        public decimal CurrentBalance { get; private set; } = 0;
+        public bool IsAccountOpen { get; private set; } = false;
+
+        private AccountAggregate() : base()
+        {}
+
+        public static AccountAggregate OpenNewAccount(Guid accountId)
+        {
+            var account = new AccountAggregate();
+            // normally opening a new account we would be generating this in another service
+            account.Id = accountId;
+            account.ApplyChange(new AccountOpenedEvent(accountId));
+            return account;
+        }
+
+        private void Apply(AccountOpenedEvent @event)
+        {
+            AccountId = @event.AccountId;
+            IsAccountOpen = true;
+            CurrentBalance = 0;
+        }
+
+        public void Deposit(Deposit deposit)
+        {
+            if(!IsAccountOpen)
+            {
+                throw new ApplicationException("Account is not open");
+            }
+
+            var depositedEvent = new AmountDepositedEvent(
+                    deposit.DepositId,
+                    deposit.Amount,
+                    deposit.Date,
+                    deposit.AccountId);
+
+            ApplyChange(depositedEvent);
+        }
+
+        private void Apply(AmountDepositedEvent @event)
+        {
+            CurrentBalance += @event.Amount;
+        }
+    }
+}
