bb98e0ab5cc19e8d7b1ff12f8c69024959e62c26
Chris Berthold <cberthold@technodotnet.com>
2020-01-08 00:02:24 -0500

    add count controller

---
+++ 2020-01-09_Building_dotnet_apps_with_docker_compose/demo2/Demo/Api/Controllers/CountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountController : ControllerBase
+    {
+        private static int current_count = 0;
+
+        [HttpGet]
+        public int Get()
+        {
+            Interlocked.Increment(ref current_count);
+            return current_count;
+        }
+    }
+}
