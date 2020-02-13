d934af95777a9f796e7797e131d727ac5dec3518
Chris Berthold <cberthold@technodotnet.com>
2020-02-13 01:38:20 -0500

    complete cleanup of new project files

--- 2020-02-13_Introduction_to_Event_Sourcing_and_CQRS/frontend/ClientApp/src/App.tsx
+++ 2020-02-13_Introduction_to_Event_Sourcing_and_CQRS/frontend/ClientApp/src/App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import FetchData from './components/FetchData';
 import Withdrawal from './components/Withdrawal';
 import Deposit from './components/Deposit';
 import BankTransactions from './components/BankTransactions';
@@ -12,7 +11,6 @@ import './custom.css'
 export default () => (
     <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
         <Route path='/transactions' component={BankTransactions} />
         <Route path='/deposit' component={Deposit} />
         <Route path='/withdrawal' component={Withdrawal} />
