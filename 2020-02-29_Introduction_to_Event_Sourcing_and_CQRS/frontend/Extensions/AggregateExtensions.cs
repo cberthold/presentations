9149c98f41682335ccfda0ffb26d8f98d1969230
Chris Berthold <cberthold@technodotnet.com>
2020-02-23 14:06:28 -0500

    change get or create account to only get account

--- 2020-02-29_Introduction_to_Event_Sourcing_and_CQRS/frontend/Extensions/AggregateExtensions.cs
+++ 2020-02-29_Introduction_to_Event_Sourcing_and_CQRS/frontend/Extensions/AggregateExtensions.cs
@@ -11,23 +11,10 @@ namespace CQRSlite.Domain
 {
     public static class AggregateExtensions
     {
-        public static async Task<AccountAggregate> GetOrCreateAccount(this ISession session, Guid accountId, CancellationToken token)
+        public static async Task<AccountAggregate> GetAccount(this ISession session, Guid accountId, CancellationToken token)
         {
-            try 
-            {
                 var account = await session.Get<AccountAggregate>(accountId, cancellationToken: token);
                 return account;
-            }
-            catch(AggregateNotFoundException ex )
-            {
-                var account = AccountAggregate.OpenNewAccount(accountId);
-                await session.Add(account, token);
-                return account;
-            }
-            catch
-            {
-                throw;
-            }
         }
     }
 }
