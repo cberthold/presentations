5ab0bd2e244894ddf3a8394fa3fe44e01c1cf449
Chris Berthold <cberthold@technodotnet.com>
2020-02-23 14:07:36 -0500

    change open new account to take our new information

--- 2020-02-29_Introduction_to_Event_Sourcing_and_CQRS/frontend/Logic/DomainAggregates/AccountAggregate.cs
+++ 2020-02-29_Introduction_to_Event_Sourcing_and_CQRS/frontend/Logic/DomainAggregates/AccountAggregate.cs
@@ -9,24 +9,47 @@ namespace frontend.Logic.DomainEvents
     public class AccountAggregate : AggregateRoot
     {
         public Guid AccountId { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string AccountType { get; private set; }
         public decimal CurrentBalance { get; private set; } = 0;
         public bool IsAccountOpen { get; private set; } = false;
+
+        private static readonly HashSet<string> ALLOWED_ACCOUNT_TYPES = 
+            new HashSet<string>(new string[]
+            {
+                "Checking",
+                "Savings",
+            });
 
         private AccountAggregate() : base()
         {}
 
-        public static AccountAggregate OpenNewAccount(Guid accountId)
+        public static AccountAggregate OpenNewAccount(
+            string firstName,
+            string lastName,
+            string accountType)
         {
+            var accountId = Guid.NewGuid();
+
+            if(!ALLOWED_ACCOUNT_TYPES.Contains(accountType))
+            {
+                throw new ApplicationException($"Account type of `{accountType}` is not an allowed account type.");
+            }
+
             var account = new AccountAggregate();
             // normally opening a new account we would be generating this in another service
             account.Id = accountId;
-            account.ApplyChange(new AccountOpenedEvent(accountId));
+            account.ApplyChange(new AccountOpenedEvent(accountId, firstName, lastName, accountType));
             return account;
         }
 
         private void Apply(AccountOpenedEvent @event)
         {
             AccountId = @event.AccountId;
+            AccountType = @event.AccountType;
+            FirstName = @event.FirstName;
+            LastName = @event.LastName;
             IsAccountOpen = true;
             CurrentBalance = 0;
         }
