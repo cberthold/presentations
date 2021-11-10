51ebd30673176e18713bce9e41496ae202d18b29
Chris Berthold <cberthold@technodotnet.com>
2021-11-09 23:57:10 -0500

    handle payments on queue service

--- 2021-11-11-Building-event-driven-architectures/src/ShoppingCart.Payments/Startup.cs
+++ 2021-11-11-Building-event-driven-architectures/src/ShoppingCart.Payments/Startup.cs
@@ -1,7 +1,9 @@
 ﻿
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ShoppingCart.Logic.Clients;
+using ShoppingCart.Logic.Services;
 using ShoppingCart.Payments;
 using System;
 using System.Collections.Generic;
@@ -16,6 +18,20 @@ namespace ShoppingCart.Payments
         public override void Configure(IFunctionsHostBuilder builder)
         {
             
+            builder.Services.AddHttpClient<PaymentClient>((client) =>
+            {
+                client.BaseAddress = new Uri("http://localhost:7071/api/");
+            });
+
+            builder.Services.AddScoped<IPaymentService, PaymentService>();
+            builder.Services.AddSingleton<OrderTableClient>((svc) =>
+            {
+                var config = svc.GetRequiredService<IConfiguration>();
+                var client = new OrderTableClient(config["PaymentConnection"]);
+                client.CreateIfNotExists();
+                return client;
+            });
+
             builder.Services.AddHttpClient<PaymentClient>((client) =>
             {
                 client.BaseAddress = new Uri("http://localhost:7071/api/");
