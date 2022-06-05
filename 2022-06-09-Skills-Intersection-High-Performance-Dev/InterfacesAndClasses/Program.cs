209ec8bbe4cc680e9b18d6a05b520f8ac27b1126
Chris Berthold <cberthold@technodotnet.com>
2022-06-04 22:23:05 -0400

    Add interface and classes skill

---
+++ 2022-06-09-Skills-Intersection-High-Performance-Dev/InterfacesAndClasses/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace InterfacesAndClasses
+{
+
+    public interface IRun { void Run(); }
+    public interface ISwim { void Swim(); }
+    public interface ITalk { void Talk(); }
+
+    public class Person : IRun, ISwim, ITalk
+    {
+        public void Run()
+        {
+            Console.WriteLine("Person Runs with Two Legs");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Person Swims using the Breaststroke");
+        }
+
+        public void Talk()
+        {
+            Console.WriteLine("Person Talks with their Mouth");
+        }
+    }
+
+    public class Shark : ISwim
+    {
+        public void Swim()
+        {
+            Console.WriteLine("Shark Swims using fins and tail");
+        }
+    }
+
+
+    public class Dog : IRun, ISwim
+    {
+        public void Run()
+        {
+            Console.WriteLine("Dog Runs with Four Legs");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Dog Swims using doggy paddle");
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            var dog = new Dog();
+            var shark = new Shark();
+
+            MakeThemRunSwimAndOrTalk(person);
+            MakeThemRunSwimAndOrTalk(dog);
+            MakeThemRunSwimAndOrTalk(shark);
+        }
+
+        private static void MakeThemRunSwimAndOrTalk(object o)
+        {
+            string className = o.GetType().Name;
+
+            Console.WriteLine($"Going to make the {className} Run Swim And/Or Talk");
+
+            if(o is IRun iCanRunThen)
+            {
+                Console.WriteLine($"Going to make the {className} Run since they can run");
+                iCanRunThen.Run();
+            }
+            else
+            {
+                Console.WriteLine($"{className} doesn't know how to Run");
+            }
+
+            if (o is ISwim iCanSwimThen)
+            {
+                Console.WriteLine($"Going to make the {className} Swim since they can swim");
+                iCanSwimThen.Swim();
+            }
+            else
+            {
+                Console.WriteLine($"{className} doesn't know how to Swim");
+            }
+
+            if (o is ITalk iCanTalkThen)
+            {
+                Console.WriteLine($"Going to make the {className} Talk since they can talk");
+                iCanTalkThen.Talk();
+            }
+            else
+            {
+                Console.WriteLine($"{className} doesn't know how to Talk");
+            }
+        }
+    }
+}
