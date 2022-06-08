07921326a23e21f14f2be2d1a55f5834423b28f2
Chris Berthold <cberthold@technodotnet.com>
2022-06-08 00:01:46 -0400

    Create composite pattern

---
+++ 2022-06-09-Skills-Intersection-High-Performance-Dev/PatternsComplete/CompositeTaxCalculator.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatternsComplete
+{
+    public class CompositeTaxCalculator : ICalculateTax
+    {
+        public IEnumerable<ICalculateTax> Calculators { get; }
+
+        public CompositeTaxCalculator(IEnumerable<ICalculateTax> calculators)
+        {
+            Calculators = calculators ?? Enumerable.Empty<ICalculateTax>();
+        }
+
+        public void CalculateTax(InvoiceContext context)
+        {
+            decimal totalTax = 0m;
+            context.Invoice.Tax = 0m;
+
+            foreach (var calc in Calculators)
+            {
+                calc.CalculateTax(context);
+                totalTax += context.Invoice.Tax;
+                context.Invoice.Tax = 0m;
+            }
+
+            context.Invoice.Tax = totalTax;
+        }
+    }
+}
---
+++ 2022-06-09-Skills-Intersection-High-Performance-Dev/PatternsComplete/ProductATaxFreeTaxCalculator.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatternsComplete
+{
+    public class ProductATaxFreeTaxCalculator : ICalculateTax
+    {
+        public void CalculateTax(InvoiceContext context)
+        {
+            var productATotal = context.Cart.Items.Where(a => a.Description == "Product A").Sum(a => a.ItemTotal);
+            var tax = Math.Round(productATotal * 0.06m, 2);
+
+            if (tax > 0)
+            {
+                context.Invoice.Tax = -1 * tax;
+            }
+        }
+    }
+}
